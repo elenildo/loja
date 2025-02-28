29da943e07a392567cc6c6c9dd9417348880ed34
elenildo <elenildoms@gmail.com>
2025-02-27 22:21:50 -0300

    changed chunk size to 100

--- src/main/java/com/elenildo/loja/config/BatchConfig.java
+++ src/main/java/com/elenildo/loja/config/BatchConfig.java
@@ -64,7 +64,7 @@ public class BatchConfig {
     public Step step1(JobRepository jobRepository, DataSourceTransactionManager transactionManager,
                       FlatFileItemReader<ProductCsvDto> reader, ProductItemProcessor processor, JdbcBatchItemWriter<ProductCsvDto> writer) {
         return new StepBuilder("step1", jobRepository)
-                .<ProductCsvDto, ProductCsvDto>chunk(5, transactionManager)
+                .<ProductCsvDto, ProductCsvDto>chunk(100, transactionManager)
                 .reader(reader)
                 .processor(processor)
                 .writer(writer)
